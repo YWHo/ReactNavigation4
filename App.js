572ed3434a5544143579e5a07cc33da720b8a753
Brian Ho <YWHo@users.noreply.github.com>
2021-03-16 00:46:22 +1300

    Navigate to a route multiple times using 'navigation.push'

--- App.js
+++ App.js
@@ -22,6 +22,10 @@ class DetailsScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
       </View>
     );
   }
