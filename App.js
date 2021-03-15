09c60f3c7617d1e31abb2e75b435fbb35fa6fbf7
Brian Ho <YWHo@users.noreply.github.com>
2021-03-15 17:01:36 +1300

    adding a second route

--- App.js
+++ App.js
@@ -13,8 +13,19 @@ class HomeScreen extends React.Component {
   }
 }
 
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
 const AppNavigator = createStackNavigator({
   Home: HomeScreen,
+  Details: DetailsScreen
 });
 
 const AppContainer = createAppContainer(AppNavigator);
